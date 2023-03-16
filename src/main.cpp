43eb01d96f5573d534f66b776567af662984b8c5
tardhiansyah <kiih.kadal@gmail.com>
2023-03-16 10:39:24 +0700

    remove unnecessary serial2.begin statement

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 
 #define RX2_PIN     16
 #define TX2_PIN     17
-#define BAUDRATE_2  9600
+#define BAUDRATE_2  9600    // usually the sensor use baudrate 9600
 
 // Communicating with sensor with Serial2 (HardwareSerial) of ESP32
 EC_TDS sensor1(Serial2, RX2_PIN, TX2_PIN, BAUDRATE_2);
@@ -21,25 +21,24 @@ PH_Temperature sensor2(Serial2, RX2_PIN, TX2_PIN, BAUDRATE_2);
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial2.begin(9600);    // most of the sensor use baudrate 9600
+  Serial.begin(115200); 
 
-    sensor1.begin(0x02, 0x03, 0x00, 0x02);
-    sensor2.begin(0x01, 0x03, 0x00, 0x02);
+  sensor1.begin(0x02, 0x03, 0x00, 0x02);
+  sensor2.begin(0x01, 0x03, 0x00, 0x02);
 }
 
 void loop()
 {
-    float tds = sensor1.readValue();
-    float temperature = sensor2.readTemperature();
-    float pH = sensor2.readPH();
+  float tds = sensor1.readValue();
+  float temperature = sensor2.readTemperature();
+  float pH = sensor2.readPH();
 
-    Serial.print("TDS: ");
-    Serial.println(tds);
-    Serial.print("Temperature: ");
-    Serial.println(temperature);
-    Serial.print("pH: ");
-    Serial.println(pH);
+  Serial.print("TDS: ");
+  Serial.println(tds);
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  Serial.print("pH: ");
+  Serial.println(pH);
 
-    delay(500);
+  delay(500);
 }
